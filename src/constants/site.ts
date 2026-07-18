3455e98b2d238099e41626af3122fae15de4b40a
Dev Talan <chaudhary.dev.talan@gmail.com>
2026-07-18 23:07:27 +0530

    brand: configure app identity and domains

---
+++ src/constants/site.ts
@@ -0,0 +1,12 @@
+export const SITE_CONFIG = {
+  name: "EnvKit",
+  logo: "/envkit-mark.svg",
+  daymLabs: {
+    name: "Daym Labs",
+    url: "https://damnlabs.com",
+  },
+  pixelactStudio: {
+    name: "Pixelact Studio",
+    url: "https://pixelactstudio.com",
+  },
+} as const
